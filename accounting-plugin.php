56e19f895b08362bb98b00e7f8754fee41c03a7e
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2025-09-06 18:09:37 +0330

    config project

--- accounting-plugin.php
+++ accounting-plugin.php
@@ -1,1 +1,52 @@
 <?php
+/*
+ * Plugin Name:       accounting
+ * Plugin URI:        https://github.com/farzane-na/accounting-plugin
+ * Description:       A plugin with which you can create online pay slips and also through this plugin you can have an Elementor widget to filter a series of products or post types.
+ * Version:           1.0.0
+ * Requires PHP:      7.4
+ * Author:            Farzane Nazmabadi
+ * Author URI:        https://farzanenazmabadi.ir/
+ * Update URI:        https://github.com/farzane-na/accounting-plugin
+ * Text Domain:       accounting
+ * Domain Path:       /languages
+ * Requires Plugins:  Elementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+function translate_plugin() {
+    load_plugin_textdomain( 'accounting', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'translate_plugin' );
+
+add_action( 'elementor/init', 'init_custom_elementor_widgets' );
+function init_custom_elementor_widgets() {
+    require_once __DIR__ . '/widgets/filtering-card-widget.php';
+
+    add_action( 'elementor/widgets/widgets_registered', 'register_custom_widget_elementor' );
+}
+function register_custom_widget_elementor( $widgets_manager ) {
+    $widgets_manager->register( new \Filtering_Card_Widget() );
+}
+
+function filtering_card_enqueue_assets() {
+
+    wp_register_style(
+        'filtering-card-style',
+        plugin_dir_url(__FILE__) . 'assets/style/app.css',
+        [],
+        '1.0.0'
+    );
+
+
+    wp_register_script(
+        'filtering-card-script',
+        plugin_dir_url(__FILE__) . 'assets/script/app.js',
+        [],
+        '1.0.0',
+        true
+    );
+}
+add_action('wp_enqueue_scripts', 'filtering_card_enqueue_assets');
---
+++ widgets/filtering-card-widget.php
@@ -0,0 +1,26 @@
+<?php
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+use Elementor\Group_Control_Typography;
+use Elementor\Group_Control_Border;
+use Elementor\Group_Control_Box_Shadow;
+use Elementor\Group_Control_Background;
+use Elementor\Group_Control_Responsive;
+use Elementor\Group_Control_Box_Shado;
+use Elementor\Utils;
+use Elementor\Scheme_Typography;
+use Elementor\Scheme_Color;
+use Elementor\Repeate;
+use Elementor\Controls_Stack;
+use Elementor\Base\Data_Control;
+use Elementor\Plugin;
+use Elementor\Frontend;
+use Elementor\Editor;
+use Elementor\Element_Bas;
+use Elementor\Group_Control_Css_Filter;
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+class Filtering_Card_Widget extends \Elementor\Widget_Base{
+
+};
